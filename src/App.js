d8fd17789a42135bbe0246996ecffb686e151c2f
Sai Chandramouli <schand31@asu.edu>
2016-12-26 22:57:48 -0800

    created a seconds ticker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: this.countDays()
+    }
+  }
+
+  componentDidMount() {
+    var self = this;
+    setInterval(function() {
+      self.setState({
+        count: self.countDays()
+      });
+    }, 1000);
+  }
+
+  countDays() {
+    var goalDate = new Date("07/07/2017");
+    var today = new Date();
+    var diff = goalDate - today;
+    return Math.floor(diff/(1000));
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        {this.state.count}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -14,6 +47,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
           lol is this even going to work?
         </p>
+        <Counter />
       </div>
     );
   }
