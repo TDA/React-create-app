daa52a9e5707372cf70402c7a5ed69adcadf7c35
Sai Chandramouli <schand31@asu.edu>
2016-12-29 23:43:22 -0800

    working inputs

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import './App.css';
 // Goals should be addable with dates for each, maybe using localstorage or something?
 // Goals can be SLA'd, i.e. when they are close to deadline, we should have color changes and stuff
 
-class GoalInput extends React.Component {
+class Goals extends React.Component {
   constructor(props) {
     super(props);
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {goals: [], name: '', date: ''};
+    this.state = {goals: [], name: '', completionDate: ''};
   }
 
   render() {
@@ -23,7 +23,7 @@ class GoalInput extends React.Component {
         <h3>Add Goal</h3>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleNameChange} value={this.state.name} />
-          <input onChange={this.handleDateChange} value={this.state.date} />
+          <input onChange={this.handleDateChange} value={this.state.completionDate} />
           <button>{'Add #' + (this.state.goals.length + 1) + ' Goal'}</button>
         </form>
       </div>
@@ -35,69 +35,56 @@ class GoalInput extends React.Component {
   }
 
   handleDateChange(e) {
-    this.setState({date: e.target.value});
+    this.setState({completionDate: e.target.value});
   }
 
   handleSubmit(e) {
     e.preventDefault();
     let newGoal = {
       name: this.state.name,
-      date: this.state.date
+      completionDate: this.state.completionDate
     };
     this.setState((prevState) => ({
       goals: prevState.goals.concat(newGoal),
       name: '',
-      date: ''
+      completionDate: ''
     }));
   }
 }
 
 class GoalsDisplayComponent extends Component {
-  constructor(props) {
-    // this is only to hold state of a goal, technically we can just have used .props
-    // to access that same data, as we don't modify state here at all
-    super(props);
-    this.state = {
-      goals: props.goals
-    }
-  }
-
   render() {
+    let goals = this.props.goals;
     return (
-      this.state.goals.map(function (goal) {
-        return <div className="goal">
-          <div className="goal-name">
-            {goal.name}
-          </div>
-          <div className="goal-date">
-            <Counter endDate={goal.goalCompletionDate}/>
-          </div>
-        </div>
-      })
+      <div>
+        {
+          goals.map(function (goal) {
+            return (
+              <div className="goal">
+                <div className="goal-name">
+                  {goal.name}
+                </div>
+                <div className="goal-date">
+                  <Counter endDate={goal.completionDate} />
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     )
   }
 }
 
 class App extends Component {
   render() {
-    var goals = [
-      {
-        name: 'Promotion',
-        date: '7/17/2017'
-      },
-      {
-        name: 'India trip',
-        date: '3/17/2017'
-      }
-    ];
-
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Goals Tracker</h2>
         </div>
-      <GoalInput />
+      <Goals />
       </div>
     );
   }
